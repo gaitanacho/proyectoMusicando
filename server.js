ecd4d0d5e569c5c1582f7f6bad3f444fda952445
gaitannacho <gaitannacho18@gmail.com>
2022-08-29 01:47:55 -0300

    Probando Server

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require ("express")
+const mysql= require("mysql")
+const myconn = require ("express-myconnection")
+
+
+
+
+const app = express()
+app.set("port", process.env.PORT || 3002)
+const dbOptions = {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "abc123",
+    database: "musicando"
+}
+
+
+
+
+// middlewares -----------------------------
+app.use(myconn(mysql, dbOptions, "single"))
+
+// rutas -----------------------------------
+app.get("/", (req, res)=>{
+    res.send ("Bienvenido a Musicando")
+})
+app.get("/api",(req, res)=> {
+    res.send ("Probando API")
+})
+// servidor corriendo ----------------------
+app.listen(3002, ()=>{
+    console.log("Servidor corriendo en el puerto", 3002)
+})
